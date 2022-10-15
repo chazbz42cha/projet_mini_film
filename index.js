7905256624bd6aa55a634d389277799c11bbc037
chazbz42cha <cha25.bz42@gmail.com>
2022-10-15 11:19:04 +0200

    up

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-Function changefont() {
+let theme = document.querySelector(".body");
+
+
+theme.addEventListener("click", (e) => {
+
+    e.target.classList.toggle("body-dark");
+    e.target.classList.toggle("body-white");
+    //circle.classList.toggle("body-dark");
+
+});
 
 
 
-}
+
+let texte = document.querySelector(".dark-texte");
+
+
+texte.addEventListener("click", (e) => {
+
+    e.target.classList.toggle("white-texte");
+    e.target.classList.toggle("dark-texte");
+    //circle.classList.toggle("body-dark");
+
+});
